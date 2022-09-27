def4a3c8e6d1f181f82090ac25ebdc5104064694
ANURAG SINGH <103880648+devilanurag@users.noreply.github.com>
2022-09-28 02:05:11 +0530

    Add files via upload

---
+++ sum.C
@@ -0,0 +1,13 @@
+//wap to take two no from the user and find sum....
+#include<stdio.h>
+#include<conio.h>
+ void main()
+{
+ int a,b,sum;
+ clrscr();
+ printf("\nenter the value of two number:");
+ scanf("%d%d",&a,&b);
+ sum=a+b;
+ printf("sum=%d",sum);
+ getch();
+}
