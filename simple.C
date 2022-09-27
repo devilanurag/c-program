1a329dcc0cb25b1f8622877ce2d4afe7dd27c041
ANURAG SINGH <103880648+devilanurag@users.noreply.github.com>
2022-09-28 02:04:23 +0530

    Add files via upload

---
+++ simple.C
@@ -0,0 +1,10 @@
+//wap to print hello world or anything you want ....
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ clrscr();
+ printf("my name is anurag singh\n");
+ printf("my father name is amrendra kumar singh");
+ getch();
+}
